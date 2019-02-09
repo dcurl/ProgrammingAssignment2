4f5e5accdf188f002ca174ab93d667b84a064db5
Dani Curl <Daniela.curl@outlook.com>
2019-02-08 19:41:37 -0500

    Added comments to code

--- cachematrix.R
+++ cachematrix.R
@@ -64,7 +64,11 @@ For this assignment, assume that the matrix supplied is always invertible. '''
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The first function, makeCacheMatrix creates a special "vector", which is really a list containing a function to
+## - set the value of the vector
+## - get the value of the vector
+## - set the value of the inverse
+## - get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
@@ -81,7 +85,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse of the special "vector" created with the above function. However, 
+## it first checks to see if the inverse has already been calculated. If so, it gets the inverse from the cache and skips 
+## the computation. Otherwise, it calculates the inverse of the matrix and sets the value of the inverse in the cache via 
+## the setinverse function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
